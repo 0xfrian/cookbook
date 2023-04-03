567f1a167aa79c93e68742122b6baa0cf52c883f
0xfrian <0xfrian@gmail.com>
2023-04-03 04:29:13 -0700

    Created basic layout using Curve Finance design, Added code for file-type input and reading in file as base64 string

---
+++ src/components/CurveContainer.tsx
@@ -0,0 +1,60 @@
+import { Flex, Heading } from "@chakra-ui/react";
+
+export default function CurveContainer({ children, heading }: any) {
+  const background: string = "#44475a";
+
+  return (
+    <Flex
+      id="outer-container"
+      flexDirection="column" 
+      justifyContent="start" 
+      alignItems="center" 
+      margin="0px" 
+      padding="15px" 
+      width="100%" 
+      height="100%"
+      background="#44475a"
+      border="6px double white"
+      boxShadow="0 0 0 3px #44475a, 10px 10px 3px black"
+    >
+      <Flex 
+        id="inner-container"
+        position="relative"
+        flexDirection="column"
+        justifyContent="start"
+        alignItems="start"
+        margin="10px"
+        padding="15px"
+        width="100%"
+        height="100%"
+        minHeight="100px"
+        border="1px groove lightgray"
+      >
+        { /* Heading Container */ }
+        <Flex 
+          position="absolute" 
+          top="-10px"
+          flexDirection="row" 
+          justifyContent="center" 
+          alignItems="center" 
+          width="100%"
+        >
+          <Heading 
+            position="relative"
+            fontWeight="400" 
+            fontSize="14px" 
+            padding="0px 8px" 
+            background="#44475a"
+          >
+            {heading}
+          </Heading>
+        </Flex>
+
+        { /* React component children */ }
+        {children}
+      </Flex>
+    </Flex>
+  );
+}
+
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,90 @@
+// Components
+import {
+  Flex,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
+import Head from "next/head";
+import CurveContainer from "../components/CurveContainer";
+
+// Hooks
+import { useEffect, useState } from "react";
+
+// Types
+import { ChangeEvent } from "react";
+
 export default function index() {
+  const [data, setData] = useState<string | ArrayBuffer>();
+
+  async function handleFiles(event: ChangeEvent<HTMLInputElement>) {
+    // Define files
+    const files: FileList = event.target.files!;
+    const image_file: File = files[0];
+
+    // Store image file as base64 string
+    const reader = new FileReader();
+    reader.readAsDataURL(image_file);
+    reader.onload = () => setData(reader.result!);
+  }
+
+  useEffect(() => {
+    if (data) console.log("Base64 String: ", data);
+  }, [data]);
+
   return (
-    <div>Hello from our home page!</div>
+    <>
+      <Head>
+        <title>Kitchen</title>
+        <link
+          rel="icon"
+          type="image/x-icon"
+          href="https://em-content.zobj.net/thumbs/120/google/350/man-cook_1f468-200d-1f373.png"
+        />
+        <meta name="viewport" content="viewport-fit=cover" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      {/* Main Container */}
+      <main>
+        <Flex
+          id="root-container"
+          flexDirection="column"
+          justifyContent="start"
+          alignItems="center"
+          position="relative"
+          width="100%"
+          minHeight="100vh"
+        >
+          <Flex
+            flexDirection="column"
+            justifyContent="start"
+            alignItems="center"
+            padding="40px 0"
+            width="100%"
+            maxWidth="800px"
+          >
+            <CurveContainer heading="AWS S3">
+              <FormLabel htmlFor="sample-image" >
+                Upload an image
+              </FormLabel>
+              <Input
+                id="sample-image"
+                type="file"
+                margin="0"
+                padding="0"
+                width="min-content"
+                height="min-content"
+                fontSize="14px"
+                border="none"
+                borderRadius="0"
+                multiple={false}
+                onChange={handleFiles}
+              />
+            </CurveContainer>
+          </Flex>
+        </Flex>
+      </main>
+    </>
   );
 }
 
